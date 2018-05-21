e5b7879b8c8d4f853ca8c1b7fab1926427c87585
den7777 <dnijebortsev@mail.ru>
2018-05-21 17:54:53 +0600

    RT-0001 add interfaces

--- src/main/java/io/khasang/rtrail/controller/AppController.java
+++ src/main/java/io/khasang/rtrail/controller/AppController.java
@@ -1,15 +1,21 @@
 package io.khasang.rtrail.controller;
 
+import io.khasang.rtrail.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController {
+    @Autowired
+
+    private Message message;
 
     @RequestMapping("/")
-    public String getHelloPage(Model model){
-        model.addAttribute("name", "World!");
+    public String getHelloPage(Model model) {
+        model.addAttribute("name", message.getInfo());
         return "index";
     }
 }
