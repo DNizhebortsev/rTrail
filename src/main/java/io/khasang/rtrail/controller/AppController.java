c41266066678b1cfd3bf354552e16ec2878e23f5
den7777 <dnijebortsev@mail.ru>
2018-05-22 20:23:14 +0600

    RT-0001 added a query to create a table

--- src/main/java/io/khasang/rtrail/controller/AppController.java
+++ src/main/java/io/khasang/rtrail/controller/AppController.java
@@ -1,5 +1,6 @@
 package io.khasang.rtrail.controller;
 
+import io.khasang.rtrail.model.CreateTable;
 import io.khasang.rtrail.model.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,12 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AppController {
     @Autowired
-
     private Message message;
+
+    @Autowired
+    private CreateTable createTable;
 
     @RequestMapping("/")
     public String getHelloPage(Model model) {
         model.addAttribute("name", message.getInfo());
         return "index";
+    }
+    @RequestMapping("/create")
+    public String getCreateTable(Model model){
+       model.addAttribute("status", createTable.createTableStatus());
+        return "create";
     }
 }
